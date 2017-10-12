4b8586714ff649ada38fe3aad2da9dbbe71cf0c8
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-12 09:40:13 -0600

    Created sufficient information for the first milestone

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # personal-website-project
+A website designed to appeal to potential employers.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Calder Benjamin</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>Calder Benjamin</h1>
+		</header>
+
+		<main>
+			<h2>Purpose</h2>
+			<p>I seek to be working on an interesting project that I am passionate about, with people
+			that hold ideals and passions compatible with mine.</p>
+
+			<h2>Audience</h2>
+			<p>Smaller and startup companies, along with potential customers looking for a freelance developer,
+			as well as other freelance developers looking for partners.</p>
+
+			<h2>Goal</h2>
+			<p>To work or intern with a project with interesting implications and goals, or to survive until
+			I reach that point.</p>
+
+			<h2>Persona</h2>
+			<h3>Name</h3>
+			<p>Shannon Digs</p>
+
+			<h3>General Information</h3>
+			<ul>
+				<li>26 years old</li>
+				<li>Lead of a project</li>
+				<li>Needs and uses a variety of coding languages and skillsets</li>
+				<li>Open minded, passionate</li>
+				<li>Recruiting developers to realize their dream</li>
+				<li>Has difficulty finding others who share their passion</li>
+				<li>Those who do share passion don't have the necessary skillset</li>
+			</ul>
+		</main>
+	</body>
+</html>
