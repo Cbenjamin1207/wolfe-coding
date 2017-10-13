cb308c4d4906d53e6528b7ba90319bba1e353e81
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-13 08:49:38 -0600

    Refined the persona section

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,30 +13,35 @@
 		<main>
 			<h2>Purpose</h2>
 			<p>I seek to be working on an interesting project that I am passionate about, with people
-			that hold ideals and passions compatible with mine.</p>
+				that hold ideals and passions compatible with mine. </p>
 
 			<h2>Audience</h2>
 			<p>Smaller and startup companies, along with potential customers looking for a freelance developer,
-			as well as other freelance developers looking for partners.</p>
+				as well as other freelance developers looking for partners.</p>
 
 			<h2>Goal</h2>
 			<p>To work or intern with a project with interesting implications and goals, or to survive until
-			I reach that point.</p>
+				I reach that point.</p>
 
 			<h2>Persona</h2>
-			<h3>Name</h3>
-			<p>Shannon Digs</p>
+			<p>Name: Shannon Digs</p>
 
-			<h3>General Information</h3>
-			<ul>
-				<li>26 years old</li>
-				<li>Lead of a project</li>
-				<li>Needs and uses a variety of coding languages and skillsets</li>
-				<li>Open minded, passionate</li>
-				<li>Recruiting developers to realize their dream</li>
-				<li>Has difficulty finding others who share their passion</li>
-				<li>Those who do share passion don't have the necessary skillset</li>
-			</ul>
+			<p>Age: 26 years old</p>
+
+			<p>Description: After finishing their degree, worked in retail for some time. Doesn't enjoy that work
+				very much, unsurprisingly, and wants to expand. Has a hearing impaired cousin, and remembers how much
+				difficulty they could sometimes have in interacting with certain websites. Shannon gets in touch with
+				them, and starts to write up a plan for a program that could help make it easier in difficult circumstances.</p>
+
+			<p>Situation: Has been working for some time in retail, but recently came up with an idea to help
+				the hearing impaired interact with websites that normally require audio cues. Shannon is now looking for passionate
+				workers with relevant skillsets to help realize the project.</p>
+
+			<p>Difficulties: Has little to no coding experience, and doesn't know the technical requirements necessary
+				to make the project possible. Most others who share the same level of passion don't bring any relevant
+				skillsets to the table, and just want to help in the planning phase. Shannon is ready to move out of the
+				planning phase and into development, but needs to find people who know how to do that.</p>
+
 		</main>
 	</body>
 </html>
