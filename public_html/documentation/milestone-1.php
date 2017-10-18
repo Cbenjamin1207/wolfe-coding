47f5c2287bc5f4fabf7f17472255971bf2a79025
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-18 08:29:00 -0600

    Updated Persona to be further along

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -28,24 +28,26 @@
 			<p>Age: 26 years old</p>
 
 			<p>Technical Expertise: Is confident in graphic design, as well as a competent mac OS 10 user,
-				but lacking any hard-coding skills.</p>
+				with a basic understanding of java and C++.</p>
 
-			<p>Attitude: Very driven, has been burnt out by retail work Shannon is very ready to be working on
+			<p>Attitude: Very driven, has been burnt out by retail work. Shannon has begun work on
 				their goal, and is looking to hire people with a similar drive. Disparages those who don't work hard,
 				and values individuality. Shannon is not looking for someone who will simply work, but rather innovate.</p>
 
-			<p>Description: After finishing their degree, worked in retail for some time. Doesn't enjoy that work
+			<p>Description: After finishing their degree, worked in retail for some time. Didn't enjoy that work
 				very much, unsurprisingly, and wants to expand their horizons. Has a hearing impaired cousin, and remembers how much
 				difficulty they could sometimes have in interacting with certain websites. Shannon gets in touch with
-				them, and starts to write up a plan for a program that could help make it easier in difficult circumstances.</p>
+				them, and starts to write up a plan for a program that could help make it easier in difficult circumstances.
+				After a basic blueprint, began work on the program, but quickly realized that it would require more than
+				just one person.</p>
 
-			<p>Situation: Has been working for some time in retail, but recently came up with an idea to help
+			<p>Situation: Has been working for some time in retail, but recently began work on an idea to help
 				the hearing impaired interact with websites that normally require audio cues. Shannon is now looking for passionate
-				workers with relevant skillsets to help realize the project.</p>
+				workers with relevant skill sets to help realize the project.</p>
 
-			<p>Frustrations: Has little to no coding experience, and doesn't know the technical requirements necessary
+			<p>Frustrations: Has little coding experience, and doesn't know the technical requirements necessary
 				to make the project possible. Most others who share the same level of passion don't bring any relevant
-				skillsets to the table, and just want to help in the planning phase. Shannon is ready to move out of the
+				skills to the table, and just offered help in the planning phase. Shannon has begun moving out of the
 				planning phase and into development, but needs to find people who know how to do that.</p>
 
 		</main>
--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,23 +11,27 @@
 			<h1>Calder Benjamin: Milestone 2</h1>
 		</header>
 
-		<h2>Content Strategy</h2>
-		<p>The overall strategy will be to showcase different projects of increasing difficulty,
-			as each will build upon the last, thus making me immediately marketable to those who
-			don't need any hard coding database design and just want a website, and then going
-			further with each section.</p>
-		<h3>Websites</h3>
-		<p>The main content I will be posting is websites that I have completed, both professional and
-			for personal enjoyment. This will be useful if I want to do freelance work, as a way to showcase
-			my work.</p>
+		<main>
+			<h2>Content Strategy</h2>
+			<p>The overall strategy will be to showcase different projects of increasing difficulty,
+				as each will build upon the last, thus making me immediately marketable to those who
+				don't need any hard coding database design and just want a website, and then going
+				further with each section.</p>
 
-		<h3>Applications/Programs</h3>
-		<p>This is to show any programs I have created, for more complicated purposes than just a basic
-			website design and layout. I may include websites that have more complicated database design
-			or javascript in here as well.</p>
+			<h3>Websites</h3>
+			<p>The main content I will be posting is websites that I have completed, both professional and
+				for personal enjoyment. This will be useful if I want to do freelance work, as a way to showcase
+				my work.</p>
 
-		<h3>About Me</h3>
-		<p>The final section will give a general overview of myself, some of my ideologies, and my
-		general life story. I will also include links to other social media and contact sites.</p>
+			<h3>Applications/Programs</h3>
+			<p>This is to show any programs I have created, for more complicated purposes than just a basic
+				website design and layout. I may include websites that have more complicated database design
+				or javascript in here as well.</p>
+
+			<h3>About Me</h3>
+			<p>The final section will give a general overview of myself, some of my ideologies, and my
+			general life story. I will also include links to other social media and contact sites.</p>
+
+		</main>
 	</body>
 </html>
